eda9e82877c577308d36d34e8a9a3bb77f94fc39
Ian Martin <martini@alum.mit.edu>
2013-10-16 02:01:13 -0400

    Overhaul register definitions.

--- cpu/rl78/uart0.c
+++ cpu/rl78/uart0.c
@@ -1,15 +1,16 @@
+#include "sfrs.h"
+#include "sfrs-ext.h"
+
 #include "uart0.h"
-#include <iodefine.h>
-#include <iodefine_ext.h>
 
 void uart0_init(void)
 {
     /* Reference R01AN0459EJ0100 or hardware manual for details */
-    PIOR.pior = 0U;                                         /* Disable IO redirection */
-    PM1.pm1 |= 0x06U;                                       /* Set P11 and P12 as inputs */
+    PIOR = 0U;                                         /* Disable IO redirection */
+    PM1 |= 0x06U;                                       /* Set P11 and P12 as inputs */
     SAU0EN = 1;                                             /* Supply clock to serial array unit 0 */
-    SPS0.sps0 = 0x44U;                                      /* Set input clock (CK00 and CK01) to fclk/16 = 2MHz */
-    ST0.st0 = 0x03U;                                        /* Stop operation of channel 0 and 1 */
+    SPS0 = 0x44U;                                      /* Set input clock (CK00 and CK01) to fclk/16 = 2MHz */
+    ST0 = 0x03U;                                        /* Stop operation of channel 0 and 1 */
     /* Setup interrupts (disable) */
     STMK0 = 1;                                              /* Disable INTST0 interrupt */
     STIF0 = 0;                                              /* Clear INTST0 interrupt request flag */
@@ -24,14 +25,14 @@ void uart0_init(void)
     SREPR10 = 1;                                            /* Set INTSRE0 priority: lowest */
     SREPR00 = 1;
     /* Setup operation mode for transmitter (channel 0) */
-    SMR00.smr00 = 0x0023U;                                  /* Operation clock : CK00,
+    SMR00 = 0x0023U;                                  /* Operation clock : CK00,
                                                                Transfer clock : division of CK00
                                                                Start trigger : software
                                                                Detect falling edge as start bit
                                                                Operation mode : UART
                                                                Interrupt source : buffer empty
                                                             */
-    SCR00.scr00 = 0x8097U;                                  /* Transmission only
+    SCR00 = 0x8097U;                                  /* Transmission only
                                                                Reception error interrupt masked
                                                                Phase clock : type 1
                                                                No parity
@@ -39,20 +40,20 @@ void uart0_init(void)
                                                                1 stop bit
                                                                8-bit data length
                                                             */
-    SDR00.sdr00 = 0xCE00U;                                  /* transfer clock : operation clock divided by 208
+    SDR00 = 0xCE00U;                                  /* transfer clock : operation clock divided by 208
                                                                2 MHz / 208 = ~9600 bps
                                                             */
     /* Setup operation mode for receiver (channel 1) */
-    NFEN0.nfen0 |= 1;                                       /* Enable noise filter on RxD0 pin */
-    SIR01.sir01 = 0x0007U;                                  /* Clear error flags */
-    SMR01.smr01 = 0x0122U;                                  /* Operation clock : CK00
+    NFEN0 |= 1;                                       /* Enable noise filter on RxD0 pin */
+    SIR01 = 0x0007U;                                  /* Clear error flags */
+    SMR01 = 0x0122U;                                  /* Operation clock : CK00
                                                                Transfer clock : division of CK00
                                                                Start trigger : valid edge on RxD pin
                                                                Detect falling edge as start bit
                                                                Operation mode : UART
                                                                Interrupt source : transfer end
                                                             */
-    SCR01.scr01 = 0x4097U;                                  /* Reception only
+    SCR01 = 0x4097U;                                  /* Reception only
                                                                Reception error interrupt masked
                                                                Phase clock : type 1
                                                                No parity
@@ -60,28 +61,28 @@ void uart0_init(void)
                                                                1 stop bit
                                                                8-bit data length
                                                             */
-    SDR01.sdr01 = 0xCE00U;                                  /* transfer clock : operation clock divided by 208
+    SDR01 = 0xCE00U;                                  /* transfer clock : operation clock divided by 208
                                                                2 MHz / 208 = ~9600 bps
                                                             */
-    SO0.so0 |= 1;                                           /* Prepare for use of channel 0 */
-    SOE0.soe0 |= 1;
-    P1.p1 |= (1 << 2);                                      /* Set TxD0 high */
-    PM1.pm1 &= ~(1 << 2);                                   /* Set output mode for TxD0 */
-    PM1.pm1 |= (1 << 1);                                    /* Set input mode for RxD0 */
-    SS0.ss0 |= 0x03U;                                       /* Enable UART0 operation (both channels) */
+    SO0 |= 1;                                           /* Prepare for use of channel 0 */
+    SOE0 |= 1;
+    P1 |= (1 << 2);                                      /* Set TxD0 high */
+     PM1 &= ~(1 << 2);                                   /* Set output mode for TxD0 */
+     PM1 |= (1 << 1);                                    /* Set input mode for RxD0 */
+    SS0 |= 0x03U;                                       /* Enable UART0 operation (both channels) */
     STIF0 = 1;                                              /* Set buffer empty interrupt request flag */
 }
 
 void uart0_putchar(int c) {
 	while (0 == STIF0);
 	STIF0 = 0;
-	SDR00.sdr00 = c;
+	SDR00 = c;
 }
 
 int uart0_puts(const char* s)
 {
     int len = 0;
-    SMR00.smr00 |= 0x0001U;                                 /* Set buffer empty interrupt */
+    SMR00 |= 0x0001U;                                 /* Set buffer empty interrupt */
     while ('\0' != *s)
     {
       uart0_putchar(*s);
@@ -93,7 +94,7 @@ int uart0_puts(const char* s)
     STIF0 = 0;
     SDR00.sdr00 = '\r';
 #endif
-    SMR00.smr00 &= ~0x0001U;
+    SMR00 &= ~0x0001U;
     uart0_putchar('\n');
 #if 0
     while (0 != SSR00.BIT.bit6);                            /* Wait until TSF00 == 0 */
